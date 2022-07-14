a810ff7d70ed0e749d8139fb92e9d036ace2532d
AlejandroDisofic <alejandrocerrato@disofic.com>
2022-07-14 16:36:21 +0200

    Environment arreglado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  hostApi: 'http://10.10.254.103:9001',
 };
 
 /*
